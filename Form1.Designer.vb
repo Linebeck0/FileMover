8f4a9ed3dc55db7b41edc010384b987979e467a8
witte <witte@BENJAMIN-RYZEN>
2023-06-17 13:21:27 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(95, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "E:\yt"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Base Folder"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(141, 16)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(56, 20)
+        Me.TextBox2.TabIndex = 0
+        Me.TextBox2.Text = "｜"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(138, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Seperator"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(234, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Move Files (1 Time)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(234, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Move Files (Loop)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 74)
+        Me.ProgressBar1.Maximum = 60
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(336, 20)
+        Me.ProgressBar1.TabIndex = 3
+        Me.ProgressBar1.Value = 50
+        Me.ProgressBar1.Visible = False
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Stopped"
+        Me.Label3.Visible = False
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Button3)
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Controls.Add(Me.TextBox1)
+        Me.Panel3.Controls.Add(Me.TextBox2)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Location = New System.Drawing.Point(12, 12)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(207, 43)
+        Me.Panel3.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(104, 14)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(31, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "..."
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(360, 101)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "File Mover"
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim base As String = TextBox1.Text
+        Dim seperator As String = TextBox2.Text
+        doWork(base:=base, seperator:=seperator)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If BackgroundWorker1.IsBusy Then
+            BackgroundWorker1.CancelAsync()
+            Button2.Text = "Move Files (Loop)"
+        Else
+            BackgroundWorker1.RunWorkerAsync()
+            Button2.Text = "Move Files (Stop)"
+        End If
+        Button1.Visible = Not Button1.Visible
+        Label3.Visible = Not Label3.Visible
+        ProgressBar1.Visible = Label3.Visible
+    End Sub
+    Private Sub doWork(base As String, seperator As String)
+        For Each file As String In My.Computer.FileSystem.GetFiles(base)
+            Dim myFile As New System.IO.FileInfo(file)
+            If myFile.Length = 0 OrElse myFile.LastWriteTime > Now.AddMinutes(-60) Then
+                Continue For
+            End If
+            If file.Contains(seperator) Then
+                Dim filename As String = file.ToLower
+                Label3.Invoke(Sub() Label3.Text = file)
+                filename = filename.Substring(filename.IndexOf(seperator) + 1)
+                Dim yourpath As String = base + "\" + filename
+                Dim targetfile As String = yourpath + file.Substring(file.LastIndexOf("\"))
+                If Not System.IO.Directory.Exists(yourpath) Then
+                    System.IO.Directory.CreateDirectory(yourpath)
+                End If
+                Try
+                    My.Computer.FileSystem.MoveFile(file, targetfile)
+                    System.Threading.Thread.Sleep(file.Substring(file.LastIndexOf("\")).Length * 25)
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
+            End If
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim base As String = TextBox1.Text
+        Dim seperator As String = TextBox2.Text
+        Try
+            While Not BackgroundWorker1.CancellationPending()
+                doWork(base:=base, seperator:=seperator)
+                ProgressBar1.Invoke(Sub() ProgressBar1.Value = ProgressBar1.Maximum)
+                While ProgressBar1.Value <> ProgressBar1.Minimum
+                    If Not BackgroundWorker1.CancellationPending Then
+                        DecreaseProgressBar()
+                        System.Threading.Thread.Sleep(1000)
+                    Else
+                        ProgressBar1.Invoke(Sub() ProgressBar1.Value = ProgressBar1.Minimum)
+                        Label3.Invoke(Sub() Label3.Text = "Stopped")
+                        Exit While
+                    End If
+                End While
+            End While
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Debugger.IsAttached Then
+            TextBox1.Text = "\\GVM05\yt"
+        End If
+    End Sub
+
+    Private Sub DecreaseProgressBar()
+        If ProgressBar1.Value <> ProgressBar1.Minimum Then
+            ProgressBar1.Invoke(Sub() ProgressBar1.Value -= 1)
+            Label3.Invoke(Sub() Label3.Text = "Waiting ... " & ProgressBar1.Value.ToString)
+        End If
+    End Sub
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+        DecreaseProgressBar()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Using dialog As New FolderBrowserDialog()
+            If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                TextBox1.Text = dialog.SelectedPath
+            End If
+        End Using
+    End Sub
+End Class
